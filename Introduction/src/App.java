ab4e7e3829c682016c15f083b9e393027416ae1f
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-12 13:25:16 +0530

    Literals

--- Introduction/src/App.java
+++ Introduction/src/App.java
@@ -18,5 +18,20 @@ public class App {
         int num3 = 30;
         int sum = num1 + num2 + num3;
         System.out.println(sum);
+
+
+        // Literals - A constant value which can be assigned to the variable is called as a literal.
+        byte age_1 = 22;
+        char n = 'A';
+        String name = "Akhil Shetty M";
+        float f1 = 5.6f;
+        double d1 = 4.66D;
+        long ageDino = 123456789l;
+        System.out.println(age_1);
+        System.out.println(name);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(ageDino);
+        System.out.println(name);
     }
 }
