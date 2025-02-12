bf4a5a558b6aeb6ac4163ab25abe7781ae01cc7e
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-12 13:07:25 +0530

    Simple java program

--- Introduction/src/App.java
+++ Introduction/src/App.java
@@ -5,10 +5,18 @@ public class App {
         double salary = 50000.50;
         char grade = 'A';
         boolean isEmployed = true;
-
+        long phoneNumber = 8951;
+        
         System.out.println("Age: " + age);
         System.out.println("Salary: " + salary);
         System.out.println("Grade: " + grade);
         System.out.println("Employed: " + isEmployed);
+        System.out.println("Ph. No. : " + phoneNumber);
+
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
     }
 }
