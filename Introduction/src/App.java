c3a3c1894fcc68433d28a02d892c83bd3b660c46
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-12 12:55:09 +0530

    Data types

--- Introduction/src/App.java
+++ Introduction/src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        int age = 25;
+        double salary = 50000.50;
+        char grade = 'A';
+        boolean isEmployed = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Employed: " + isEmployed);
     }
 }
