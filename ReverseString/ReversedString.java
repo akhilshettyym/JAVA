e05d8e975dff5b97ded8a432c67083c0ffdc9304
akhilshettyym <akhilshettym2003@gmail.com>
2025-02-12 15:33:36 +0530

    Reverseing a string

---
+++ ReverseString/ReversedString.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ReversedString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String str = sc.nextLine();
+        String reversed = "";
+        for (int i = str.length() - 1 ; i >= 0 ; i--)
+        {
+            reversed = reversed + str.charAt(i);
+        }
+        System.out.println("The reveresed string is :" + reversed);
+        // System.out.println(reversed);
+    }
+}
