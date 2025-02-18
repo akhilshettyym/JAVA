1bcaf61f0d2d54b617a06221562dbe084ab782ab
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-18 15:42:09 +0530

    Conditional statements else if ladder

--- ConditionalStatements/ifElse.java
+++ ConditionalStatements/ifElse.java
@@ -10,6 +10,22 @@ public class ifElse {
     } else {
         System.out.println("You are not eligible to vote.");
     }
+    
+
+    System.out.println("Enter your age :");
+    int a = sc.nextInt();
+    if (a <= 18) {
+        System.out.println("You are a fresher.");
+    }
+    else if (a >= 30){
+        System.out.println("You are a fresher with a lot of exposure.");
+    }
+    else if (a > 60){
+        System.out.println("You are a fresher with a lot of exposure and experience.");
+    }
+    else{
+        System.out.println("Invalid input");
     sc.close();
+    }
 }
 }
