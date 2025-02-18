561ae2b551d2685234ca07741b7513b9fcf15415
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-18 15:46:37 +0530

    Conditional statements updated

---
+++ ConditionalStatements/elseIf.java
@@ -0,0 +1,24 @@
+package ConditionalStatements;
+import java.util.Scanner;
+
+public class elseIf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age :");
+        int a = sc.nextInt();
+        if (a <= 18) {
+            System.out.println("You are a fresher.");
+        }
+        else if (a >= 30){
+            System.out.println("You are a fresher with a lot of exposure.");
+        }
+        else if (a > 60){
+            System.out.println("You are a fresher with a lot of exposure and experience.");
+        }
+        else{
+            System.out.println("Invalid input");
+        }
+        sc.close();
+    }    
+}
+
