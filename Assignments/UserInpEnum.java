0b2081aa3acb48dfde5647fcad47d4ccaf5d0ed1
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-20 17:17:01 +0530

    Enums user input

--- Assignments/UserInpEnum.java
+++ Assignments/UserInpEnum.java
@@ -2,10 +2,10 @@ package Assignments;
 import java.util.Scanner;
 
 enum Day {
-    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;
+    SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY;         // Fixed constants
 
     public boolean isWeekend() {
-        return this == SATURDAY || this == SUNDAY;
+        return this == SATURDAY || this == SUNDAY;                          // Method checks wtr the day is weekend (Sat or sun)
     }
 }
 
@@ -14,17 +14,21 @@ public class UserInpEnum {
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a day of the week: ");
-        String input = sc.nextLine().trim().toUpperCase(); // Convert input to uppercase
+        String input = sc.nextLine().trim().toUpperCase();          // Convert input to uppercase and trim cuz if extar spaces gievn by user
 
         try {
-            Day day = Day.valueOf(input); // Convert user input to Enum
-            String formattedDay = day.name().charAt(0) + day.name().substring(1).toLowerCase();
+            Day day = Day.valueOf(input); // Converting user inp to Enum 
+            // that is if input from user is Monday then  |||   day = Day.Monday
 
-            System.out.println(String.format("%s is a %s.", 
-                formattedDay, 
-                day.isWeekend() ? "weekend" : "weekday"
+            String formattedDay = day.name().charAt(0) + day.name().substring(1).toLowerCase();
+            // This will first take the name using day. and it'll obtain the first letter say M from Monday
+            // Then the rest of the string will be converted to lowercase that is from index 1.
+
+            System.out.println(String.format("%s is a %s.", formattedDay, // for first %s wll return the formatted day 
+                day.isWeekend() ? "weekend" : "weekday"                          // This will check wtr the day is weekday or weekend from above
             ));
-        } catch (IllegalArgumentException e) {
+
+        } catch (IllegalArgumentException e) {          // if user inputs sanday/ funday the Day.valueOf(input will throw an error) this is caught by catch block.
             System.out.println("Invalid input! Please enter a valid day.");
         }
         
