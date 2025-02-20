221f811c9af71da2dc1fb1446e1dbd4b0cdec625
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-20 15:24:38 +0530

    Sum of even numbers

---
+++ PracticeSets/Sum.java
@@ -0,0 +1,12 @@
+package PracticeSets;
+
+public class Sum {
+    public static void main(String[] args) {
+        int Sum = 0;
+        int n = 3;
+        for(int i = 0; i < n; i++){
+            Sum = Sum + (2*i);
+        }
+        System.out.println("Sum of first " + n + "even natural numbers is " + Sum);
+    }
+}
