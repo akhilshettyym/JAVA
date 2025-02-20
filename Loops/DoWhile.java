923a019c81325735b2b005b4212a125bb5ade298
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-20 12:51:51 +0530

    Do while Loops

---
+++ Loops/DoWhile.java
@@ -0,0 +1,30 @@
+package Loops;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        // Demonstrating while loop
+        int a = 0;
+        while (a < 5) { // While loop checks condition first
+            System.out.println(a);
+            a++;
+        }
+        
+        // Demonstrating do-while loop
+        int b = 10; // Even if b starts at 10, the loop executes at least once
+        do {
+            System.out.println(b);
+            b++;
+        } while (b < 5);
+        
+        // Print first n natural numbers using do-while loop
+        int n = 10;
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= n);
+    }
+}
+
+// While checks the condition first before executing the code.
+// Do-while executes the code at least once before checking the condition.
