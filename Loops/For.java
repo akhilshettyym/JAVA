b2805b336272b9ddcba27508485ad19ae383e265
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-20 13:04:46 +0530

    For Loops

---
+++ Loops/For.java
@@ -0,0 +1,38 @@
+package Loops;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        // Demonstrating while loop
+        int a = 0;
+        while (a < 5) { // While loop checks condition first
+            System.out.println(a);
+            a++;
+        }
+        
+        // Demonstrating do-while loop
+        int b = 10; // Even if b starts at 10, the loop executes at least once
+        do {
+            System.out.println(b);
+            b++;
+        } while (b < 5);
+        
+        // Print first n natural numbers using do-while loop
+        int n = 10;
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= n);
+        
+        // Print odd numbers using for loop
+        System.out.println("Odd numbers from 1 to 10:");
+        for (int j = 1; j <= 10; j++) {
+            if (j % 2 != 0) {
+                System.out.println(j + " is an Odd number");
+            }
+        }
+    }
+}
+
+// While checks the condition first before executing the code.
+// Do-while executes the code at least once before checking the condition.
