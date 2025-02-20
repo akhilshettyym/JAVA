c7d60c66fbd7e511080f254718187a47cb5f5a55
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-20 16:29:00 +0530

    Weekday and Weekend problem fixed

--- Assignments/DayEnd.java
+++ Assignments/DayEnd.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 public class DayEnd {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out("Enter a day of a week :");
+        System.out.print("Enter a day of a week :");
         String day = sc.nextLine().trim().toLowerCase();
 
         switch(day){
-            case "Monday":
-            case "Tuesday":
-            case "Wednesday":
-            case "Thursday":
-            case "Friday":
-                System.out.println(day + "is a weekday.");
+            case "monday":
+            case "tuesday":
+            case "wednesday":
+            case "thursday":
+            case "friday":
+                System.out.println(day + " is a weekday.");
                 break;
 
-            case "Saturday":
-            case "Sunday":
-                System.out.println(day + "is a weekend.");
+            case "saturday":
+            case "sunday":
+                System.out.println(day + " is a weekend.");
                 break;
             
-            // default:
-            //     System.out.println("Invalid input!!! Please enter a valid day of the week ");
+            default:
+                System.out.println("Invalid input!!! Please enter a valid day of the week ");
         }
         sc.close();
     }
