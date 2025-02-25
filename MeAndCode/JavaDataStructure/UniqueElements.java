028960d02627e750ee4fc1ba85782fcbeea4d2e9
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-25 12:36:26 +0530

    Unique Element in array list

---
+++ MeAndCode/JavaDataStructure/UniqueElements.java
@@ -0,0 +1,31 @@
+package MeAndCode.JavaDataStructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniqueElements {
+    public static void main(String[] args) {
+        ArrayList<String> list1 = new ArrayList<>();
+        list1.add("car");
+        list1.add("bike");
+        list1.add("bus");
+        list1.add("train");
+        list1.add("truck");
+
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("car");
+        list2.add("bike");
+        list2.add("train");
+        list2.add("truck");
+
+        ArrayList<String> newList = new ArrayList<String>();
+
+        for (String item : list1){
+            if (!list2.contains(item)){
+                newList.add(item);
+            }
+        }
+        // System.out.println("New Array List with unique elements :" + newList);
+        System.out.println("NewList = " + newList);
+    }
+}
