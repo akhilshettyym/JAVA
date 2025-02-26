070deb2277026441fa0c82d515f8774389b251ca
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-26 13:01:17 +0530

    Structure commit

---
+++ MeAndCode/JavaAdvanced/JavaCollections_09.java
@@ -0,0 +1,5 @@
+package MeAndCode.JavaAdvanced;
+import java.util.Collections;
+public class JavaCollections_09 {
+
+}
---
+++ MeAndCode/JavaAdvanced/JavaComparator_10.java
@@ -0,0 +1,6 @@
+package MeAndCode.JavaAdvanced;
+
+public class JavaComparator_10 {
+
+}
+// Sort an ArrayList in ascending order
