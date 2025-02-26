2de9eb6c468c3a29ab7da9e85f2b152fa1d5d0b1
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-26 12:08:52 +0530

    Java TreeMap

---
+++ MeAndCode/JavaDataStructure/JavaTreeMap_07.java
@@ -0,0 +1,14 @@
+package MeAndCode.JavaDataStructure;
+import java.util.TreeMap;
+public class JavaTreeMap_07 {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> number = new TreeMap<Integer, String>();
+        number.put(1, "One");
+        number.put(3,"Three");
+        number.put(4, "Four");
+        number.put(5, "Five");
+        number.put(2, "Two");
+        System.out.println(number);
+    }
+}
+// Automatically sort in order according to the keys
