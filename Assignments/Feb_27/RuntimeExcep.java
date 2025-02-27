425b7bd8a08c5123a64b7674eb2cf83d95b320f0
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-02-27 17:25:11 +0530

    Duplicate numbers

--- Assignments/Feb_27/RuntimeExcep.java
+++ Assignments/Feb_27/RuntimeExcep.java
@@ -1,12 +1,7 @@
 package Assignments.Feb_27;
 
-/**
- * Java Program to Demonstrate Throwing Multiple Runtime Exceptions
- * This program intentionally throws different runtime exceptions 
- * like ArithmeticException, NullPointerException, ArrayIndexOutOfBoundsException, 
- * ArrayStoreException, and ClassCastException.
- * 
- * 3. write a program to throw all the runtime exceptions like ArithmeticException, NullPointerException, ArrayIndexOutOfBoundException, ArrayStoreException and ClassCastException.
+/*
+  3. write a program to throw all the runtime exceptions like ArithmeticException, NullPointerException, ArrayIndexOutOfBoundException, ArrayStoreException and ClassCastException.
  */
 
 public class RuntimeExcep {
