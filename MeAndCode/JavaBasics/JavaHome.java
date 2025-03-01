33604f15ea09500ee41dfeaba816e5300f0fe814
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-03-01 21:39:13 +0530

    Java basic program intro

---
+++ MeAndCode/JavaBasics/JavaHome.java
@@ -0,0 +1,12 @@
+package MeAndCode.JavaBasics;
+
+public class JavaHome {
+    /*Learn Java
+    Java is a popular programming language.
+    
+    Java is used to develop mobile apps, web apps, desktop apps, games and much more.
+*/
+    public static void main(String[] args) {
+        System.out.println("Java is a popular programming language.");
+    }
+}
