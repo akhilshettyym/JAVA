1060e24b704ba64faf27fef41d79df7f5c72bf35
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-03-09 20:13:53 +0530

    Outputs in java

--- MeAndCode/01_JavaBasics/Output_04.java
+++ MeAndCode/01_JavaBasics/Output_04.java
@@ -1,3 +1,20 @@
-public class Output_04 {
+// Java Output Example - Demonstrating println() and print() Methods
 
+public class Output_04 {
+    public static void main(String[] args) {
+
+        // Using println() - Prints text and moves to a new line after each statement
+        System.out.println("Hello World!"); 
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        
+        // Demonstrating missing double quotes (Uncommenting the below line will cause an error)
+        // System.out.println(This sentence will produce an error);
+
+        // Using print() - Prints text but does not move to a new line
+        System.out.print("Hello World! "); 
+        System.out.print("I will print on the same line."); 
+
+        // Note: We added an extra space in "Hello World! " to improve readability
+    }
 }
