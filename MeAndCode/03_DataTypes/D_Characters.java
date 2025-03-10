286c4ab84e69524d530f65873423477e47e04c42
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-03-10 14:15:21 +0530

    DataType Characters

--- MeAndCode/03_DataTypes/D_Characters.java
+++ MeAndCode/03_DataTypes/D_Characters.java
@@ -1,3 +1,29 @@
-public class D_Characters {
+// Java Characters and Strings - Example
 
+public class D_Characters {
+    public static void main(String[] args) {
+
+        // Character variables
+        char myGrade = 'B';
+        System.out.println("My grade is: " + myGrade);
+
+        // ASCII values to characters
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println("ASCII to Characters:");
+        System.out.println(myVar1); // A
+        System.out.println(myVar2); // B
+        System.out.println(myVar3); // C
+
+        System.out.println("----------------------------");
+
+        // String variable
+        String greeting = "Hello, World!";
+        System.out.println("Greeting message: " + greeting);
+
+        // String concatenation
+        String firstName = "John";
+        String lastName = "Doe";
+        String fullName = firstName + " " + lastName;
+        System.out.println("Full Name: " + fullName);
+    }
 }
