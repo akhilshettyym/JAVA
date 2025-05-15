533c2fe74467b21653747e5bab3bb6c7edc8ac88
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-05-15 14:24:56 +0530

    While loop

---
+++ MyCode/10_WhileLoop/D_Example_2.java
@@ -0,0 +1,23 @@
+public class Example {
+    public static void main(String[] args) {
+        int dice = 1;
+
+        while (dice <= 6) {
+            if (dice < 6) {
+                System.out.println("No Yatzy.");
+            } else {
+                System.out.println("Yatzy!");
+            }
+            dice = dice + 1;
+        }
+    }
+}
+
+/*
+No Yatzy.
+No Yatzy.
+No Yatzy.
+No Yatzy.
+No Yatzy.
+Yatzy !
+ */
