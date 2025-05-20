94cb7c1d221e0d7c2b458fa8ab15afeb32043b2a
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-05-20 21:38:29 +0530

    Methods update

--- MyCode/B_JavaMethods/A_Methods.java
+++ MyCode/B_JavaMethods/A_Methods.java
@@ -1,23 +1,19 @@
 public class Main {
 
-    // This is a user-defined method named 'myMethod'
-    // 'static' means this method belongs to the class, not instances (objects)
-    // 'void' means this method does not return a value
-    static void Method() {
-      // This line will be executed when the method is called
+  // This is a user-defined method named 'myMethod'
+  // 'static' means this method belongs to the class itself, not objects
+  // 'void' means this method does not return any value
+  static void myMethod() {
+      // This line will be executed each time the method is called
       System.out.println("I just got executed!");
-    }
-  
-    // This is the main method: the starting point of any Java program
-    public static void main(String[] args) {
-      
-      // Calling the method for the first time
+  }
+
+  // This is the main method: the starting point of any Java program
+  public static void main(String[] args) {
+
+      // Calling the method multiple times
       myMethod(); // Output: I just got executed!
-      
-      // Calling the method again (second time)
       myMethod(); // Output: I just got executed!
-      
-      // Calling the method one more time (third time)
       myMethod(); // Output: I just got executed!
-    }
-  }  
+  }
+}
