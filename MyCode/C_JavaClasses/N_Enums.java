e00f84c10a4c65368af74580468a23c0993b00ed
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-06-05 18:33:29 +0530

    Enums

---
+++ MyCode/C_JavaClasses/N_Enums.java
@@ -0,0 +1,35 @@
+public class Main {
+
+  // Define enum inside the class
+  enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+  }
+
+  public static void main(String[] args) {
+
+    // Accessing enum constant
+    Level myVar = Level.MEDIUM;
+    System.out.println("Current level: " + myVar);
+
+    // Using enum in switch statement
+    switch (myVar) {
+      case LOW:
+        System.out.println("Low level");
+        break;
+      case MEDIUM:
+        System.out.println("Medium level");
+        break;
+      case HIGH:
+        System.out.println("High level");
+        break;
+    }
+
+    // Looping through enum values
+    System.out.println("All levels:");
+    for (Level level : Level.values()) {
+      System.out.println(level);
+    }
+  }
+}
