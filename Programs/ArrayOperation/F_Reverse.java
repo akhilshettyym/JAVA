dcf370c3cc569059ad507862d3aabb65da4d5f5d
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-06-08 21:41:00 +0530

    Reversing an array

---
+++ Programs/ArrayOperation/F_Reverse.java
@@ -0,0 +1,10 @@
+public class F_Reverse{
+    public static void main(String[] args) {
+        int[] num = { 4, 8, 12, 16, 20};
+
+        System.out.println("Reversed array : ");
+        for (int i = num.length -1; i >= 0; i--){
+            System.out.println(num[i] + "");
+        }
+    }
+}
