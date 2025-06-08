038005a48d7aee57f7bfc014b817ee546e5555de
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-06-08 20:49:29 +0530

    Printing all elements from an array

---
+++ Programs/ArrayOperation/A_PrintAllEle.java
@@ -0,0 +1,10 @@
+public class A_PrintAllEle {
+    public static void main(String[] args){
+        int[] num = {10, 20, 30, 40, 50};
+
+        System.out.println("Array Elements are :");
+        for(int n : num){
+            System.out.println(n);
+        }
+    }
+}
