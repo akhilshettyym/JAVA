33e94b947505fd6ac69413f9f85356e65c9ebb20
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-02 21:12:08 +0530

    Product of two number inp

---
+++ Java_Basics/Basic_Part_1/ProdOfTwo.java
@@ -0,0 +1,14 @@
+package akhilshettyym.JAVA.Java_Basics.Basic_Part_1;
+
+import java.util.Scanner;
+
+public class ProdOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input first number :");
+        int num1 = sc.nextInt();
+        System.out.println("Input second number :");
+        int num2 = sc.nextInt();
+        System.out.println("Sum of num1 and num2 :" + (num1*num2));
+    }
+}
