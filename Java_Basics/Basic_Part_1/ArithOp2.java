d259787693cc202727b3ac0a93b1898433cc23b7
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-02 21:17:17 +0530

    Overall commit

---
+++ Java_Basics/Basic_Part_1/ArithOp2.java
@@ -0,0 +1,16 @@
+package akhilshettyym.JAVA.Java_Basics.Basic_Part_1;
+import java.util.Scanner;
+public class ArithOp2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input first number :");
+        int num1 = sc.nextInt();
+        System.out.println("Input second number :");
+        int num2 = sc.nextInt();
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+        System.out.println(num1/num2);
+        System.out.println(num1%num2);
+    }
+}
