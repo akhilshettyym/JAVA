eb060ad300ac8f1e6501fc4313ef276817848c73
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-02 20:48:55 +0530

    Sum of Two numbers

---
+++ Java_Basics/Basic_Part_1/SumOfTwo.java
@@ -0,0 +1,7 @@
+package akhilshettyym.JAVA.Java_Basics.Basic_Part_1;
+
+public class SumOfTwo {
+    public static void main(String[] args) {
+        System.out.println("74 + 36 ="+ (74+36));
+    }
+}
