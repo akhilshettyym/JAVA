b16492fcf680d6f391d090544c261bca09ea602c
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-03 20:49:32 +0530

    Multiplication of a number

---
+++ Java_Basics/Basic_Part_1/G_Multi.java
@@ -0,0 +1,13 @@
+package akhilshettyym.JAVA.Java_Basics.Basic_Part_1;
+import java.util.Scanner;
+public class G_Multi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        for(int i=0; i<10; i++){
+            System.out.println(num + "x" + (i+1) + "=" + num*(i+1));
+        }
+        sc.close();
+    }
+}
