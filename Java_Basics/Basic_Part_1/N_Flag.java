642129003b339d1b45968f1f1db2f11f71cb430a
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-07 19:13:36 +0530

    Flag display

---
+++ Java_Basics/Basic_Part_1/N_Flag.java
@@ -0,0 +1,18 @@
+package akhilshettyym.JAVA.Java_Basics.Basic_Part_1;
+
+public class N_Flag {
+    public static void main(String[] args) {
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        // Print a row of equal signs to complete the design
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+    }
+}
+
