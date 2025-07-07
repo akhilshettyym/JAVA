ef06046bd3a78ec56789f44bf30a2c8c69b66a90
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-07 21:17:23 +0530

    Add two binary numbers

--- Java_Basics/Basic_Part_1/Q_AddBin.java
+++ Java_Basics/Basic_Part_1/Q_AddBin.java
@@ -3,36 +3,32 @@ import java.util.Scanner;
 
 public class Q_AddBin {
     public static void main(String[] args) {
-        long binary1, binary2;
         int i = 0, remainder = 0;
+        long bin1, bin2;
         int[] sum = new int[20];
+        Scanner sc = new Scanner(System.in);
 
-        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the First Binary number :");
+        bin1 = sc.nextLong();
+        System.out.print("Enter the Second Binary number :");
+        bin2 = sc.nextLong();
 
-        System.out.print("Input first binary number: ");
-        binary1 = in.nextLong();
-
-        System.out.print("Input second binary number: ");
-        binary2 = in.nextLong();
-
-        while (binary1 != 0 || binary2 != 0) {
-            sum[i++] = (int) ((binary1 % 10 + binary2 % 10 + remainder) % 2);
-            remainder = (int) ((binary1 % 10 + binary2 % 10 + remainder) / 2);
-            binary1 = binary1 / 10;
-            binary2 = binary2 / 10;
+        while (bin1 != 0 || bin2 != 0) {
+            sum[i++] = (int) ((bin1 % 10 + bin2 % 10 + remainder) % 2);
+            remainder = (int) ((bin1 % 10 + bin2 % 10 + remainder) / 2);
+            bin1 = bin1 / 10;
+            bin2 = bin2 / 10;
         }
 
         if (remainder != 0) {
             sum[i++] = remainder;
         }
-        --i;
+        i--;
 
-        System.out.print("Sum of two binary numbers: ");
+        System.out.println("The sum of two Binary numbers is :");
         while (i >= 0) {
             System.out.print(sum[i--]);
         }
-
-        System.out.print("\n");
-        in.close();
+        sc.close();
     }
 }
