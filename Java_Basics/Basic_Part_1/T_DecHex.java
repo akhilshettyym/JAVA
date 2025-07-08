8192f6806147c1f222fc90a2c3f726007d0badca
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-08 22:04:43 +0530

    Decimal to Hexadecimal

--- Java_Basics/Basic_Part_1/T_DecHex.java
+++ Java_Basics/Basic_Part_1/T_DecHex.java
@@ -3,21 +3,20 @@ import java.util.Scanner;
 
 public class T_DecHex {
     public static void main(String args[]) {
-        int dec, rem;
-        String hexdec = "";
-        char hex[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
         Scanner sc = new Scanner(System.in);
+        int rem;
+        String hexdec = "";
+        char hex[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 
-        System.out.print("Input a decimal number: ");
-        dec = sc.nextInt();
+        System.out.print("Enter a Decimal Number : ");
+        int dec = sc.nextInt();
 
-        while (dec > 0) {
+        while(dec > 0){
             rem = dec % 16;
             hexdec = hex[rem] + hexdec;
             dec = dec / 16;
         }
-
-        System.out.print("Hexadecimal number is: " + hexdec + "\n");
         sc.close();
+        System.out.println("Hexadecimal number is :"+hexdec);
     }
 }
