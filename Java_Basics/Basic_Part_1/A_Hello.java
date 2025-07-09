7da995039e0b1c93e64f3bea9041d6c579c1c91a
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-09 21:50:05 +0530

    Removed comment

--- Java_Basics/Basic_Part_1/A_Hello.java
+++ Java_Basics/Basic_Part_1/A_Hello.java
@@ -1,6 +1,5 @@
 package akhilshettyym.JAVA.Java_Basics.Basic_Part_1;
 
-// Write a Java program to print 'Hello' on screen and your name on a separate line.
 public class A_Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
