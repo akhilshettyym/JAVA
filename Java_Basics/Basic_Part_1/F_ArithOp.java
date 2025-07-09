b646675eba469696c72e6587fb7d6b9a124c29ce
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-09 21:50:56 +0530

    Formatted

--- Java_Basics/Basic_Part_1/F_ArithOp.java
+++ Java_Basics/Basic_Part_1/F_ArithOp.java
@@ -1,5 +1,6 @@
 package akhilshettyym.JAVA.Java_Basics.Basic_Part_1;
 import java.util.Scanner;
+
 public class F_ArithOp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
