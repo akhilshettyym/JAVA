a1c12d2133cd0983374eb3eb4825349ad2d88a9b
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-14 22:06:40 +0530

    Sum of integers

---
+++ Java_Basics/Basic_Part_1/Z_Sum.java
@@ -0,0 +1,24 @@
+package akhilshettyym.JAVA.Java_Basics.Basic_Part_1;
+import java.util.Scanner;
+
+public class Z_Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input an integer: ");
+
+        long n = sc.nextLong();
+
+        System.out.println("The sum of the digits is: " + sumDigits(n));
+        sc.close();
+    }
+
+    public static int sumDigits(long n) {
+        int sum = 0;
+        while (n != 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+}
