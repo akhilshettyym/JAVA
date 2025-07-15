6ebd70a4c9f9646afb912dd516007898766fc100
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-15 20:58:50 +0530

    Overall commit

---
+++ DSA/Arrays/H_IsSorted.java
@@ -0,0 +1,18 @@
+package akhilshettyym.JAVA.DSA.Arrays;
+
+public class H_IsSorted {
+    public static void main(String[] args) {
+        int[] arr = new int[] { 3, 5, 1, 8, 9 };
+        boolean isSorted = checkSorted(arr);
+        System.out.println("Is array sorted :" + isSorted);
+    }
+
+    public static boolean checkSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
