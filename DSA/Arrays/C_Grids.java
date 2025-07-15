6d9f1e97e8ab21f4f937a6d7765057adee6957c0
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-15 19:16:27 +0530

    Grids, Avg, Reverse

---
+++ DSA/Arrays/C_Grids.java
@@ -0,0 +1,13 @@
+package akhilshettyym.JAVA.DSA.Arrays;
+
+public class C_Grids {
+    public static void main(String[] args) {
+        int[][] a= new int[10][10];
+
+        for (int i=0; i<10; i++){
+            for (int j=0; j<10; j++){
+                System.out.printf("%2d ", a[i][j]);
+            }
+        }
+    }
+}
---
+++ DSA/Arrays/E_Reverse.java
@@ -0,0 +1,26 @@
+package akhilshettyym.JAVA.DSA.Arrays;
+
+public class E_Reverse {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+
+        reverseArray(arr);
+        System.out.println("Reversed array :");
+        for(int num : arr) {
+            System.out.println(num+ "");
+        }
+    }
+    public static void reverseArray(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
