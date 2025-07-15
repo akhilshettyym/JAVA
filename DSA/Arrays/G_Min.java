c21728c2ee8e4255783f274f3f0c6a8f2adbc2dd
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-15 19:59:17 +0530

    Minimum number in an array

--- DSA/Arrays/G_Min.java
+++ DSA/Arrays/G_Min.java
@@ -4,17 +4,17 @@ public class G_Min {
     public static void main(String[] args) {
         int[] arr = { 2, 3, 4, 5, 1 };
 
-        int max = findMax(arr);
-        System.out.println("The maximum number in an array is :" + max);
+        int min = findMin(arr);
+        System.out.println("The minimum number in an array is :" + min);
     }
 
-    public static int findMax(int[] arr) {
-        int max = arr[0];
+    public static int findMin(int[] arr) {
+        int min = arr[0];
         for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < max) {
-                max = arr[i];
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        return max;
+        return min;
     }
 }
