474602a0666ae9f04b8ec2657330ca7946b47a07
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-16 16:16:58 +0530

    Two sums

--- DSA/Arrays/M_Dups.java
+++ DSA/Arrays/M_Dups.java
@@ -2,24 +2,26 @@ package akhilshettyym.JAVA.DSA.Arrays;
 
 public class M_Dups {
     public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3, 1, 4, 5, 6};
+        int[] arr = new int[] { 1, 2, 3, 1, 4, 5, 6 };
         int dups = removeDuplicate(arr);
-        
+
         System.out.print("The array after the removal of dups :");
-        for (int i = 0; i<dups; i++){
+        for (int i = 0; i < dups; i++) {
             System.out.print(arr[i] + "");
         }
     }
-    public static int removeDuplicate(int[] arr){
-        if(arr.length == 0) return 0;
-        
+
+    public static int removeDuplicate(int[] arr) {
+        if (arr.length == 0)
+            return 0;
+
         int i = 0;
-        for(int j=1; j<arr.length; j++){
-            if (arr[j] !=arr[i]){
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[j] != arr[i]) {
                 i++;
                 arr[i] = arr[j];
             }
         }
-        return i+1;
+        return i + 1;
     }
 }
