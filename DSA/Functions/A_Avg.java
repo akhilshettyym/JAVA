51f1317bfed280981d093a12c0c0837b13347c7f
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-29 14:54:08 +0530

    Avg and greater number among two

--- DSA/Functions/A_Avg.java
+++ DSA/Functions/A_Avg.java
@@ -2,20 +2,19 @@ package akhilshettyym.JAVA.DSA.Functions;
 import java.util.Scanner;
 
 public class A_Avg {
-    public static void avgOfTwo(int num1, int num2, int num3) {
-        int avg = (num1 + num2 + num3) / 3;
-        System.out.println("The average of numbers are :" + avg);
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the first number :");
-        int num1 = sc.nextInt();
-        System.out.print("Enter the second number :");
-        int num2 = sc.nextInt();
-        System.out.print("Enter the third number :");
-        int num3 = sc.nextInt();
-        avgOfTwo(num1, num2, num3);
+        System.out.print("Enter an odd number:");
+        int num = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+                if (i == num) {
+                    System.out.print("The sum of Odd numbers :" + sum);
+                }
+            }
+        }
         sc.close();
     }
 }
