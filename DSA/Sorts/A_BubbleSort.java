b73c035ee7e812ccb99f6d2b9ceebff9c19fbfba
akhilshettyym <akhilshettym2003@gmail.com>
2025-07-30 17:42:15 +0530

    Bubble sort

---
+++ DSA/Sorts/A_BubbleSort.java
@@ -0,0 +1,36 @@
+package akhilshettyym.JAVA.DSA.Sorts;
+import java.util.Scanner;
+
+public class A_BubbleSort {
+    public static void printSorted(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array :");
+        int s = sc.nextInt();
+        int arr[] = new int[s];
+        System.out.print("Enter the elements of an array :");
+        for (int i = 0; i < s; i++) {
+            if (sc.hasNextInt()) {
+                arr[i] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < s - 1; i++) {
+            for (int j = 0; j < s - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printSorted(arr);
+        sc.close();
+    }
+}
