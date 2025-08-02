f0a28b4080e9104a6bd46e95f7fdacdc9358cf0b
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-08-02 23:36:19 +0530

    formatted doc

--- Assignments/Feb_12/ReverseString.java
+++ Assignments/Feb_12/ReverseString.java
@@ -1,10 +1,10 @@
 package akhilshettyym.JAVA.Assignments.Feb_12;
+
 public class ReverseString {
     public static void main(String[] args) {
         String str = "Hello World";
         String reversed = "";
-        for (int i = str.length() - 1 ; i >= 0 ; i--)
-        {
+        for (int i = str.length() - 1; i >= 0; i--) {
             reversed = reversed + str.charAt(i);
         }
         System.out.println(reversed);
