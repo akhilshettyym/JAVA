7b00911e7fb0795e2af8aed4720477ef29bd70d2
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-04 21:46:41 +0530

    Sudoku

--- DSA/D_Recursion/X_Sudoku.java
+++ DSA/D_Recursion/X_Sudoku.java
@@ -84,7 +84,7 @@ public class X_Sudoku {
         // Print solved board
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                System.out.print(board[i][j] + " ");
+                System.out.print(board[i][j] + "  ");
             }
             System.out.println();
         }
