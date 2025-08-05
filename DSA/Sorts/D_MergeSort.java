9c4d5f59606b79c3d0dcb6df810fbedad55c7693
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-05 18:45:09 +0530

    Merge sort

---
+++ DSA/Sorts/D_MergeSort.java
@@ -0,0 +1,64 @@
+package akhilshettyym.JAVA.DSA.Sorts;
+
+import java.util.Scanner;
+
+public class D_MergeSort {
+    public static void conquer(int[] arr, int si, int mid, int ei) {
+        int merged[] = new int[ei - si + 1];
+        int idx1 = 0;
+        int idx2 = 0;
+        int x = 0;
+
+        while (idx1 <= mid && idx2 <= ei) {
+            if (arr[idx1] <= arr[idx2]) {
+                merged[x++] = arr[idx1++];
+            } else {
+                merged[x++] = arr[idx2++];
+            }
+        }
+
+        while (idx1 <= mid) {
+            merged[x++] = arr[idx1++];
+        }
+        while (idx2 <= ei) {
+            merged[x++] = arr[idx2++];
+        }
+
+        for (int i = 0, j = si; i < merged.length; i++, j++) {
+            arr[j] = merged[i];
+        }
+    }
+
+    public static void divide(int[] arr, int si, int ei) {
+        if (si >= ei) {
+            return;
+        }
+        int mid = si + (ei - si) / 2;
+        divide(arr, si, mid);
+        divide(arr, mid + 1, ei);
+        conquer(arr, si, mid, ei);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of an array :");
+        int s = sc.nextInt();
+
+        int[] arr = new int[s];
+        System.out.println("Enter the elements of an array :");
+        for (int i = 0; i < s; i++) {
+            if (sc.hasNext()) {
+                arr[i] = sc.nextInt();
+            }
+        }
+
+        divide(arr, 0, s - 1);
+
+        for (int i = 0; i < s; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
