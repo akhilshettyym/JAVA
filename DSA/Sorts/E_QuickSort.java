51c3beed3c8ba0ea45a9666c48001a52cb317d43
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-05 20:51:51 +0530

    Formatted Quick Sort and code correction

--- DSA/Sorts/E_QuickSort.java
+++ DSA/Sorts/E_QuickSort.java
@@ -3,14 +3,14 @@ package akhilshettyym.JAVA.DSA.Sorts;
 import java.util.Scanner;
 
 public class E_QuickSort {
-    public static int partition(int[] arr, int low, int high){
+    public static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
-        int i = low-1;
+        int i = low - 1;
 
-        for(int j=low; j<high; j++){
-            if(arr[j] < pivot){
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
                 i++;
-                
+
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
@@ -22,13 +22,15 @@ public class E_QuickSort {
         arr[high] = temp;
         return i;
     }
-    public static void quickSort(int[] arr, int low, int high){
-        if(low < high){
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
             int pidx = partition(arr, low, high);
-            quickSort(arr, low, pidx-1);
-            quickSort(arr, pidx+1, high);
+            quickSort(arr, low, pidx - 1);
+            quickSort(arr, pidx + 1, high);
         }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
