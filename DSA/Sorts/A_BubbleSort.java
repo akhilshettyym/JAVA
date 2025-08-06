b521deec6a16c78b24e334fe71e7c19ced73bc41
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-06 14:19:28 +0530

    Bubble sort with Comments

--- DSA/Sorts/A_BubbleSort.java
+++ DSA/Sorts/A_BubbleSort.java
@@ -1,26 +1,9 @@
 package akhilshettyym.JAVA.DSA.Sorts;
+
 import java.util.Scanner;
 
 public class A_BubbleSort {
-    public static void printSorted(int arr[]) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter the size of the array :");
-        int s = sc.nextInt();
-        int arr[] = new int[s];
-        System.out.print("Enter the elements of an array :");
-        for (int i = 0; i < s; i++) {
-            if (sc.hasNextInt()) {
-                arr[i] = sc.nextInt();
-            }
-        }
-
+    public static void Sort(int[] arr, int s) {
         for (int i = 0; i < s - 1; i++) {
             for (int j = 0; j < s - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -30,7 +13,25 @@ public class A_BubbleSort {
                 }
             }
         }
-        printSorted(arr);
+        for (int i = 0; i < s; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of an array : ");
+        int s = sc.nextInt();
+
+        int[] arr = new int[s];
+        System.out.print("Enter array elements : ");
+        for (int i = 0; i < s; i++) {
+            if (sc.hasNextInt()) {
+                arr[i] = sc.nextInt();
+            }
+        }
+        Sort(arr, s);
         sc.close();
     }
 }
