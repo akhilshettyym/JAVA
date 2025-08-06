2458ac562bfb2cd96294646f9210e8f20477239f
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-06 15:19:21 +0530

    Bubble sort comments

--- DSA/Sorts/A_BubbleSort.java
+++ DSA/Sorts/A_BubbleSort.java
@@ -36,29 +36,31 @@ public class A_BubbleSort {
     }
 }
 
-// Push largest element to the end with iterations
-// Time Complexity = O(n^2);
+/*
+Push largest element to the end with iterations
+Time Complexity = O(n^2);
 
-// First pass - for int i = 0
-// arr = {6 3 9 5 2 8}
-// - (compare 6 & 3) - (6 > 3) - swap - 3 6 9 5 2 8
-// - (compare 6 & 9) - (6 > 9) - no s - 3 6 9 5 2 8
-// - (compare 9 & 5) - (9 > 5) - swap - 3 6 5 9 2 8
-// - (compare 9 & 2) - (9 > 2) - swap - 3 6 5 2 9 8
-// - (compare 9 & 8) - (9 > 8) - swap - 3 6 5 2 8 9
+First pass - for int i = 0
+arr = {6 3 9 5 2 8}
+- (compare 6 & 3) - (6 > 3) - swap - 3 6 9 5 2 8
+- (compare 6 & 9) - (6 > 9) - no s - 3 6 9 5 2 8
+- (compare 9 & 5) - (9 > 5) - swap - 3 6 5 9 2 8
+- (compare 9 & 2) - (9 > 2) - swap - 3 6 5 2 9 8
+- (compare 9 & 8) - (9 > 8) - swap - 3 6 5 2 8 9
 
-// Second pass - for int i = 1
-// arr = {3 6 5 2 8 9}
-// - (compare 3 & 6) - (3 > 6) - no s - 3 6 5 2 8 9
-// - (compare 6 & 5) - (6 > 5) - swap - 3 5 6 2 8 9
-// - (compare 6 & 2) - (6 > 2) - swap - 3 5 2 6 8 9
-// - (compare 6 & 8) - (6 > 8) - no s - 3 5 2 6 8 9
-// - (compare 8 & 9) - (8 > 9) - no s - 3 5 2 6 8 9
+Second pass - for int i = 1
+arr = {3 6 5 2 8 9}
+- (compare 3 & 6) - (3 > 6) - no s - 3 6 5 2 8 9
+- (compare 6 & 5) - (6 > 5) - swap - 3 5 6 2 8 9
+- (compare 6 & 2) - (6 > 2) - swap - 3 5 2 6 8 9
+- (compare 6 & 8) - (6 > 8) - no s - 3 5 2 6 8 9
+- (compare 8 & 9) - (8 > 9) - no s - 3 5 2 6 8 9
 
-// Third pass - for int i = 2
-// arr = {3 5 2 6 8 9}
-// - (compare 3 & 5) - (3 > 5) - no s - 3 5 2 6 8 9
-// - (compare 5 & 2) - (5 > 2) - swap - 3 2 5 6 8 9
-// - (compare 5 & 6) - (5 > 6) - no s - 3 2 5 6 8 9
-// - (compare 6 & 8) - (6 > 8) - no s - 3 2 5 6 8 9
-// - (compare 8 & 9) - (8 > 9) - no s - 3 2 5 6 8 9
+Third pass - for int i = 2
+arr = {3 5 2 6 8 9}
+- (compare 3 & 5) - (3 > 5) - no s - 3 5 2 6 8 9
+- (compare 5 & 2) - (5 > 2) - swap - 3 2 5 6 8 9
+- (compare 5 & 6) - (5 > 6) - no s - 3 2 5 6 8 9
+- (compare 6 & 8) - (6 > 8) - no s - 3 2 5 6 8 9
+- (compare 8 & 9) - (8 > 9) - no s - 3 2 5 6 8 9
+ */
