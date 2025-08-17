4e9a9ab01735eb2d350c57d99ed5b71e36417695
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-08-17 23:55:37 +0530

    Formatted doc

--- MyCode/A_JavaTut/06_Strings/04_SpecialChar.java
+++ MyCode/A_JavaTut/06_Strings/04_SpecialChar.java
@@ -1,40 +1,32 @@
-public class 04_SpecialChar {
-    public static void main(String[] args) {
-        
-        // Using double quotes inside a string using escape sequence \"
-        String txt1 = "We are the so-called \"Vikings\" from the north.";
-        System.out.println(txt1); // Output: We are the so-called "Vikings" from the north.
+public class 04_ SpecialChar{public static void main(String[]args){
 
-        // Using single quote inside a string using escape sequence \'
-        String txt2 = "It\'s alright.";
-        System.out.println(txt2); // Output: It's alright.
+// Using double quotes inside a string using escape sequence \"
+String txt1="We are the so-called \"Vikings\" from the north.";System.out.println(txt1); // Output: We are the so-called
+                                                                                         // "Vikings" from the north.
 
-        // Using a backslash inside a string using escape sequence \\
-        String txt3 = "The character \\ is called backslash.";
-        System.out.println(txt3); // Output: The character \ is called backslash.
+// Using single quote inside a string using escape sequence \'
+String txt2="It\'s alright.";System.out.println(txt2); // Output: It's alright.
 
-        // Using \n for new line
-        String txt4 = "First line\nSecond line";
-        System.out.println(txt4);
-        // Output:
-        // First line
-        // Second line
+// Using a backslash inside a string using escape sequence \\
+String txt3="The character \\ is called backslash.";System.out.println(txt3); // Output: The character \ is called
+                                                                              // backslash.
 
-        // Using \t for a tab space
-        String txt5 = "Name:\tAkhil";
-        System.out.println(txt5); // Output: Name:    Akhil
+// Using \n for new line
+String txt4="First line\nSecond line";System.out.println(txt4);
+// Output:
+// First line
+// Second line
 
-        // Using \r (carriage return)
-        String txt6 = "Hello\rWorld!";
-        System.out.println(txt6); // Output: World! (may overwrite Hello depending on system)
+// Using \t for a tab space
+String txt5="Name:\tAkhil";System.out.println(txt5); // Output: Name: Akhil
 
-        // Using \b (backspace)
-        String txt7 = "Helloo\b!";
-        System.out.println(txt7); // Output: Hello!
+// Using \r (carriage return)
+String txt6="Hello\rWorld!";System.out.println(txt6); // Output: World! (may overwrite Hello depending on system)
 
-        // Using \f (form feed)
-        String txt8 = "This is\fFormFeed";
-        System.out.println(txt8); // Output depends on console (may not show visible difference)
+// Using \b (backspace)
+String txt7="Helloo\b!";System.out.println(txt7); // Output: Hello!
 
-    }
-}
+// Using \f (form feed)
+String txt8="This is\fFormFeed";System.out.println(txt8); // Output depends on console (may not show visible difference)
+
+}}
