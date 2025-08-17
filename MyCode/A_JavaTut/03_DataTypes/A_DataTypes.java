79a886a542c153f3c7f26c238bf75e79bd8b5f3d
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-08-17 23:54:35 +0530

    Formatted doc

--- MyCode/A_JavaTut/03_DataTypes/A_DataTypes.java
+++ MyCode/A_JavaTut/03_DataTypes/A_DataTypes.java
@@ -4,11 +4,11 @@ public class A_DataTypes {
     public static void main(String[] args) {
 
         // Primitive Data Types
-        int myNum = 5;               // Integer (whole number)
-        float myFloatNum = 5.99f;    // Floating point number
-        char myLetter = 'D';         // Character
-        boolean myBool = true;       // Boolean
-        String myText = "Hello";     // String (Non-primitive)
+        int myNum = 5; // Integer (whole number)
+        float myFloatNum = 5.99f; // Floating point number
+        char myLetter = 'D'; // Character
+        boolean myBool = true; // Boolean
+        String myText = "Hello"; // String (Non-primitive)
 
         // Printing values of different data types
         System.out.println("Integer Value: " + myNum);
@@ -20,9 +20,9 @@ public class A_DataTypes {
         System.out.println("----------------------------");
 
         // Demonstrating other primitive data types
-        byte myByte = 100;           // Byte (-128 to 127)
-        short myShort = 1000;        // Short (-32,768 to 32,767)
-        long myLong = 10000000000L;  // Long (-9 quintillion to +9 quintillion)
+        byte myByte = 100; // Byte (-128 to 127)
+        short myShort = 1000; // Short (-32,768 to 32,767)
+        long myLong = 10000000000L; // Long (-9 quintillion to +9 quintillion)
         double myDouble = 19.987654321; // Double (higher precision floating point)
 
         // Printing more primitive data types
--- MyCode/A_JavaTut/03_DataTypes/E_Examples.java
+++ MyCode/A_JavaTut/03_DataTypes/E_Examples.java
@@ -2,15 +2,15 @@
 
 public class E_Examples {
   public static void main(String[] args) {
-      // Create variables of different data types
-      int items = 50;              // Integer (number of items)
-      float costPerItem = 9.99f;   // Floating point number (cost per item)
-      float totalCost = items * costPerItem; // Calculating total cost
-      char currency = '$';         // Character (currency symbol)
+    // Create variables of different data types
+    int items = 50; // Integer (number of items)
+    float costPerItem = 9.99f; // Floating point number (cost per item)
+    float totalCost = items * costPerItem; // Calculating total cost
+    char currency = '$'; // Character (currency symbol)
 
-      // Print variables
-      System.out.println("Number of items: " + items);
-      System.out.println("Cost per item: " + currency + costPerItem);
-      System.out.println("Total cost = " + currency + totalCost);
+    // Print variables
+    System.out.println("Number of items: " + items);
+    System.out.println("Cost per item: " + currency + costPerItem);
+    System.out.println("Total cost = " + currency + totalCost);
   }
 }
