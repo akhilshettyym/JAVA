457ac962022725a2801116ce415f132ee20579b8
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-08-17 23:52:38 +0530

    Formatted doc

--- MyCode/A_JavaTut/01_Basics/D_OutputText.java
+++ MyCode/A_JavaTut/01_Basics/D_OutputText.java
@@ -1,19 +1,18 @@
-
-
 public class D_OutputText {
     public static void main(String[] args) {
 
         // Using println() - Prints text and moves to a new line after each statement
-        System.out.println("Hello World!"); 
+        System.out.println("Hello World!");
         System.out.println("I am learning Java.");
         System.out.println("It is awesome!");
-        
-        // Demonstrating missing double quotes (Uncommenting the below line will cause an error)
+
+        // Demonstrating missing double quotes (Uncommenting the below line will cause
+        // an error)
         // System.out.println(This sentence will produce an error);
 
         // Using print() - Prints text but does not move to a new line
-        System.out.print("Hello World! "); 
-        System.out.print("I will print on the same line."); 
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
 
         // Note: We added an extra space in "Hello World! " to improve readability
     }
