508fd59cbf7e2f1d07a1a457afeb0088bd279540
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-28 16:45:04 +0530

    Test msg for amend

--- DSA/ArrayLists.java
+++ DSA/ArrayLists.java
@@ -3,7 +3,7 @@ package akhilshettyym.JAVA.DSA;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class ArrayListt {
+public class ArrayLists {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
 
