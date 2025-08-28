ce01bb10baab8355c651e05f8cfcd67528ed8720
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-28 22:28:52 +0530

    Collections imp of Queue and TwoStackQueue

--- DSA/Queues/CollectionsQueue.java
+++ DSA/Queues/CollectionsQueue.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class CollectionsQueue {
     public static void main(String[] args) {
         Queue<Integer> q = new LinkedList<>();
+        // Queue<Integer> q = new ArrayDeque<>();
 
         q.add(1);
         q.add(2);
