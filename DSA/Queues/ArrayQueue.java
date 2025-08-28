1b43de287acbb9c9fe1a612218c9ecdaeb30aa5b
akhilshettyym <akhilshettym2003@gmail.com>
2025-08-28 18:08:43 +0530

    Implementation of Queues with Array

--- DSA/Queues/ArrayQueue.java
+++ DSA/Queues/ArrayQueue.java
@@ -2,20 +2,20 @@ package akhilshettyym.JAVA.DSA.Queues;
 
 public class ArrayQueue {
     static class Queue {
-        static int arr[];
-        static int size;
-        static int rear = -1;
+        int arr[];
+        int size;
+        int rear = -1;
 
         public Queue(int n) {
             arr = new int[n];
-            Queue.size = n;
+            size = n;
         }
 
-        public static boolean isEmpty() {
+        public boolean isEmpty() {
             return rear == -1;
         }
 
-        public static void add(int data) {
+        public void add(int data) {
             if (rear == size - 1) {
                 System.out.println("Full Queue");
                 return;
@@ -24,36 +24,37 @@ public class ArrayQueue {
             arr[rear] = data;
         }
 
-        public static int remove() {
+        public int remove() {
             if (isEmpty()) {
                 System.out.println("Empty Queue");
                 return -1;
             }
             int front = arr[0];
             for (int i = 0; i < rear; i++) {
-                arr[i] = arr[i + i];
+                arr[i] = arr[i + 1];
             }
             rear--;
             return front;
         }
 
-        public static int peek() {
+        public int peek() {
             if (isEmpty()) {
                 System.out.println("Empty Queue");
                 return -1;
             }
             return arr[0];
         }
-
     }
-    public static void main(String[] args) {
-        Queue.add(1);
-        Queue.add(2);
-        Queue.add(3);
 
-        while (!Queue.isEmpty()) {
-            System.out.println(Queue.peek());
-            Queue.remove();
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
         }
     }
 }
