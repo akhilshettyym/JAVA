bf8762474e8e26f04cad55312a832cb6e1684f85
akhilshettyym <akhilshettym2003@gmail.com>
2025-09-10 17:46:52 +0530

    Pre Order Traversal

--- DSA/Advanced/Trees/PreOrder.java
+++ DSA/Advanced/Trees/PreOrder.java
@@ -16,22 +16,32 @@ public class PreOrder {
     static class BinaryTree {
         static int idx = -1;
 
-        public static Node buildTree(int nodes[]) {
+        public static Node buildNode(int nodes[]) {
             idx++;
             if (nodes[idx] == -1) {
                 return null;
             }
             Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
+            newNode.left = buildNode(nodes);
+            newNode.right = buildNode(nodes);
 
             return newNode;
         }
     }
 
+    public static void preOrder(Node root) {
+        if (root == null) {
+            System.out.print("-1 "); // Remove this line if -1 not required.
+            return;
+        }
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-        Node root = BinaryTree.buildTree(nodes);
-        System.out.println(root.data);
+        Node root = BinaryTree.buildNode(nodes);
+        preOrder(root);
     }
 }
